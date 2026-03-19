5653337ba94bcc6b8d70bf26459ab0bd78062a8d
Pedrocoverdata <pmgarbenito@gmail.com>
2026-03-19 10:13:31 +0100

    fix: update redirects to 302

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 export default {
-  content: ["./src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte}"],
+  content: [
+    "./src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte}",
+    "./public/**/*.html",
+    "./*.astro",
+    "./*.html",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
-
